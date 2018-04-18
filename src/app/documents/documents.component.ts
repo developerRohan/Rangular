05c9c55a40b95b6e8c363a45817032e382b56d5b
itsme.Rohan <jhambrohan44@gmail.com>
2018-04-18 23:00:16 +0530

    created Document interface and traverse it

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Document } from './document'
+
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html'
 }) 
 
-export class DocumentsComponent{}
+export class DocumentsComponent{
+
+  documents: Document[];
+
+  constructor(){
+    this.documents = [
+      {
+        title:"first title",
+        description:"first description",
+        file_url:"http://google.com",
+        updated_at:"11/02/18",
+        image_url:"http://google.com"
+      },
+      {
+        title:"second title",
+        description:"second description",
+        file_url:"http://google.com",
+        updated_at:"11/02/18",
+        image_url:"http://google.com"
+      },
+      {
+        title:"third title",
+        description:"third description",
+        file_url:"http://google.com",
+        updated_at:"11/02/18",
+        image_url:"http://google.com"
+      }
+    ];
+  }
+}
