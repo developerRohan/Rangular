c07f1d1a5d5e7ac8a522eda812424d64cdd0b6ba
itsme.Rohan <jhambrohan44@gmail.com>
2018-04-19 12:31:52 +0530

    created new proposal component

--- src/app/proposal/proposal.ts
+++ src/app/proposal/proposal.ts
@@ -1,8 +1,8 @@
 export class Proposal{
   constructor(
-    public id? : number,
-    public customer? : string,
-    public portfolio_url? : string,
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = "http://",
     public tools? : string,
     public estimated_hours? : number,
     public hourly_rate? : number,
