5d194604432dd5d5339ca04665cfc809614adf29
itsme.Rohan <jhambrohan44@gmail.com>
2018-04-19 19:46:05 +0530

    added proposal show component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
-import { AppRoutingModule } from './app-routing.module';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
+
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   imports: [
     BrowserModule,
